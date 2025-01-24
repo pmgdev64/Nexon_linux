229d4f501ed896d6e245a541643fe8a739522594
P€pperMCGamer$ <113534696+pmgdev64@users.noreply.github.com>
2025-01-24 19:05:29 +0700

    Update Kernel.cs

--- src/GlobalSys/Kernel.cs
+++ src/GlobalSys/Kernel.cs
@@ -1,4 +1,13 @@
 using System;
 using Sys = Cosmos.System
 
-namespace Nexon.Kernel
+namespace NexonKernel {
+  public class kernel : Sys.Kernel {
+    protected override void BeforeRun() {
+      System = Console.WriteLine("Welcome To " + ConsoleColor.Cyan + "NexonKernel");
+    }
+
+    protected override void Run() {
+      var Console = Console.WriteLine($"Root@" + Consolecolor.Cyan + "NexonKernel: ~$ ");
+      var ConsoleRead = Console.ReadLine(Console)
+  
