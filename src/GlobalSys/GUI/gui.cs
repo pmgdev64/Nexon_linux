2d7f085d8bffa1c544f46bbd1508b0c5b85e2aa5
P€pperMCGamer$ <113534696+pmgdev64@users.noreply.github.com>
2025-01-26 13:30:04 +0700

    Update gui.cs

--- src/GlobalSys/GUI/gui.cs
+++ src/GlobalSys/GUI/gui.cs
@@ -1,1 +1,11 @@
+using System;
+using System.IO
+using System.Collections.Generic;
+using Cosmos.System;
+using Cosmos.System.Graphics;
+
+namespace NexonKernel.Interface.GUI {
+    public class gui {
+    }
+}
 
