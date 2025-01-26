2339c52885cf6f2cfc4ddc379a6da4e17fb4c555
P€pperMCGamer$ <113534696+pmgdev64@users.noreply.github.com>
2025-01-26 13:44:17 +0700

    Create installer.cs

---
+++ src/GlobalSys/installer.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Cosmos.System;
+using Cosmos.Core;
+using Cosmos.HAL.BlockDevice;
+using Cosmos.System.FileSystem.Listing;
+using Cosmos.System.FileSystem.VFS;
+
+namespace NexonKernel.FileSystem.Installer {
+    public class installer {
+        public void install_system() {
+        }
+    }
+}
