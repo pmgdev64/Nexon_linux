bf7f9959c8cc68279cb56e0634be9211a4f0de34
P€pperMCGamer$ <113534696+pmgdev64@users.noreply.github.com>
2025-01-26 19:42:45 +0700

    Update installer.cs

--- src/GlobalSys/FileSystems/installer.cs
+++ src/GlobalSys/FileSystems/installer.cs
@@ -10,6 +10,27 @@ using Cosmos.System.FileSystem.VFS;
 namespace NexonKernel.FileSystem.Installer {
     public class installer {
         public void install_system() {
+            Console.Clear();
+            Console.WriteLine("YourOS will be install in your pc");
+            Console.WriteLine("Create a username and password:");
+            Console.Write("Username: ");
+            string username = Console.ReadLine();
+            Console.Write("Password: ");
+            string cPassword = Console.ReadLine();
+            Console.Write(username +"> "); //when your put username variable it should look like this "The user name: "
+            Console.WriteLine("Creating System Directory..."); //Creating Directory or folder
+            fs.CreateDirectory("0:\\yourOS\\");
+            Console.WriteLine("Creating File for user");
+            fs.CreateFile("0:\\yourOS\\System.txt"); //Creating File System
+            fs.CreateFile("0:\\yourOS\\users.db");
+            fs.CreateFile("0:\\yourOS\\password.db"); //creating a password files
+            Console.WriteLine("Setting User Preferences...");
+            File.WriteAllText("0:\\yourOS\\System.txt", write);
+            File.WriteAllText("0:\\yourOS\\users.db", username); `    //This will save username
+            File.WriteAllText("0:\\yourOS\\password.db", cPassword); //this one will save the user password
+            Console.WriteLine("Reboot ONEOS in 3 seconds");
+            Cosmos.HAL.Global.PIT.Wait(3000);
+            Sys.Power.Reboot(); //when
         }
     }
 }
